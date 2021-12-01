3a36fc12d90e2858728f2ec30551a3ada36fc6e0
Mario <mpalmava@emeal.nttdata.com>
2021-12-01 11:37:46 +0100

    First test case

---
+++ Object Repository/IndexPage/txtTeamName.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txtTeamName</name>
+   <tag></tag>
+   <elementGuidId>f209d772-5e85-4264-9fb7-c1f509f1ac25</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//header[@data-ue-model=&quot;portada portada&quot;]//ul[@class=&quot;${className}&quot;]//a</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Scripts/TestCase1/Script1638349834707.groovy
@@ -0,0 +1,18 @@
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import internal.GlobalVariable as GlobalVariable
+import pages.GenericPage
+import pages.IndexPage
+
+// open Marca in browser
+WebUI.openBrowser(GlobalVariable.URL)
+WebUI.maximizeWindow()
+
+// accept cookies
+def genericPage = new GenericPage()
+genericPage.clickAcceptCookies()
+
+// get teams and save in file
+def indexPage = new IndexPage()
+List<String> teamsNames = indexPage.getTeamsNames("icon-teams horizontal")
+genericPage.printFile(teamsNames)
